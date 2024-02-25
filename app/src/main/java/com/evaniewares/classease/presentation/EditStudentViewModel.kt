d696206fe275f09b2d441233d023ed3655ee7e39
EvanieWares <evaniewares@gmail.com>
2024-02-25 17:57:45 +0200

    Switched from using _editState.value = editState.value.copy() to _editState.update{}

--- app/src/main/java/com/evaniewares/classease/presentation/EditStudentViewModel.kt
+++ app/src/main/java/com/evaniewares/classease/presentation/EditStudentViewModel.kt
@@ -5,6 +5,7 @@ import com.evaniewares.classease.domain.model.StudentEntity
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class EditStudentViewModel : ViewModel() {
     private val _editState = MutableStateFlow(EditState())
@@ -14,29 +15,67 @@ class EditStudentViewModel : ViewModel() {
     fun onAction(userAction: UserAction){
         when(userAction){
             UserAction.AddButtonClicked -> {
+                _editState.update {
+                    it.copy(
+                        selectedStudent = null,
+                        isAdding = true
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     selectedStudent = null,
                     isAdding = true
                 )
+                */
             }
 
             is UserAction.DeleteButtonClicked -> {
+                _editState.update {
+                    it.copy(
+                        selectedStudent = userAction.studentEntity,
+                        isDeleting = true
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     selectedStudent = userAction.studentEntity,
                     isDeleting = true
                 )
+                */
             }
             is UserAction.EditButtonClicked -> {
+                _editState.update {
+                    it.copy(
+                        selectedStudent = userAction.studentEntity,
+                        isEditing = true,
+                        studentName = userAction.studentEntity.studentName,
+                        gender = userAction.studentEntity.gender,
+                        studentId = userAction.studentEntity.studentId.toString()
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     selectedStudent = userAction.studentEntity,
                     isEditing = true,
                     studentName = userAction.studentEntity.studentName,
                     gender = userAction.studentEntity.gender,
-                    studentId = userAction.studentEntity.studentId.toString(),
+                    studentId = userAction.studentEntity.studentId.toString()
                 )
+                */
             }
 
             UserAction.EditStudentDialogDismiss -> {
+                _editState.update {
+                    it.copy(
+                        isEditing = false,
+                        isAdding = false,
+                        studentName = "",
+                        gender = "",
+                        studentId = "",
+                        selectedStudent = null
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     isEditing = false,
                     isAdding = false,
@@ -45,37 +84,76 @@ class EditStudentViewModel : ViewModel() {
                     studentId = "",
                     selectedStudent = null
                 )
+                */
             }
 
             UserAction.DeleteStudentDialogDismiss -> {
+                _editState.update {
+                    it.copy(
+                        isDeleting = false,
+                        selectedStudent = null
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     isDeleting = false,
                     selectedStudent = null
                 )
+                */
             }
 
             is UserAction.OnGenderChanged -> {
+                _editState.update {
+                    it.copy(
+                        gender = userAction.gender
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     gender = userAction.gender
                 )
+                */
             }
             is UserAction.OnIDChanged -> {
+                _editState.update {
+                    it.copy(
+                        studentId = userAction.studentId
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     studentId = userAction.studentId
                 )
+                */
             }
             is UserAction.OnNameChanged -> {
+                _editState.update {
+                    it.copy(
+                        studentName = userAction.studentName
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     studentName = userAction.studentName
                 )
+                */
             }
 
             is UserAction.OnSaveStudent -> {
+                _editState.update {
+                    it.copy(
+                        studentName = "",
+                        gender = "",
+                        studentId = ""
+                    )
+                }
+                /*
                 _editState.value = _editState.value.copy(
                     studentName = "",
                     gender = "",
                     studentId = ""
                 )
+                */
             }
         }
     }
